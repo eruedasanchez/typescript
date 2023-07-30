c10bbf0eb04118b6bbbde9ce30f6fc7eff824786
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-07-29 21:37:33 -0300

    Arrays

---
+++ dist/arrays.js
@@ -0,0 +1,18 @@
+"use strict";
+/********************** ARRAYS **********************/
+const languages = []; // declaramos que languages es un array de tipo string con : string[] (recomendado)
+const languagesDos = []; // otra manera de declarar que languagesDos en un arreglo de tipo string
+const languagesTiposCombinados = []; // declaramos que languagesTiposCombinados es un array de tipo string o number
+const languagesTiposCombinadosDos = [];
+languages.push('Typescript');
+languages.push('C++');
+languagesDos.push('Javacript');
+languagesTiposCombinados.push('Python');
+languagesTiposCombinados.push(3);
+languagesTiposCombinadosDos.push('C');
+languagesTiposCombinadosDos.push(38);
+const gameBoard = [
+    ['X', 'O', 'X'],
+    ['X', 'O', 'X'],
+    ['X', 'O', 'X']
+];
